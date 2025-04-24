f15796eda76bb13df96d5ea7a5e2edee6ed2e12b
DavidQ50 <davidquerido06@gmail.com>
2025-04-24 21:38:58 +0100

    n sei

--- bubble-trouble-main/BubbleTrouble/obj/Debug/net6.0/BubbleTrouble.AssemblyInfo.cs
+++ bubble-trouble-main/BubbleTrouble/obj/Debug/net6.0/BubbleTrouble.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BubbleTrouble")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d0e1345cc7a98cb03a59dbc6f554ce36bcf8d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BubbleTrouble")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BubbleTrouble")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
